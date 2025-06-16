3b774a13767b4c752bff798b74d7d997b40c16b7
Tanay <133881515+tanayprabhakar@users.noreply.github.com>
2025-06-16 22:19:47 +0530

    Update Day1Q1.java

--- DSA/TanayPrabhakar_500119182/Day1/Day1Q1.java
+++ DSA/TanayPrabhakar_500119182/Day1/Day1Q1.java
@@ -1,7 +1,7 @@
 public class Day1Q1 {
     public int[] replaceNegatives(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < 0) {
+            f (nums[i] < 0) {
                 nums[i] = 0;
             }
         }
