752d6ce32e632ee217706734e4359f97e2b6ee2e
ab2908 <adityabhandari4002@gmail.com>
2025-06-16 22:59:13 +0530

    DAY1 solution

--- DSA/AdityaBhandari_500120150/DAY1/Q2.java
+++ DSA/AdityaBhandari_500120150/DAY1/Q2.java
@@ -1,5 +1,28 @@
 package DSA.AdityaBhandari_500120150.DAY1;
 
-public class q2 {
+class Missing{
+    public void MR(int[] nums){
+        for(int i=1;i<nums.length;i++){
+            int repeating=-1;
+            int missing=-1;
+
+            for(int j=0;j<=nums.length;i++){
+
+            }
+            
+        }
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]);
+        }
+    }
+
+}
+public class Q2{
+    public static void main(String[] args) {
+        int[] nums= {-1,2,-3,4,5};
+        Missing n= new Missing();
+        n.MR(nums);
+    }
     
 }
+
