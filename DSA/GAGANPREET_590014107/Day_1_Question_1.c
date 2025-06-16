4e3268aa661960535e45d64d82ead6f0dfa062cc
Gagan <virkgagan1322@gmail.com>
2025-06-16 15:52:57 +0530

    Create  Day_1_Question_1.c

---
+++ DSA/GAGANPREET_590014107/Day_1_Question_1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+void replaceNegativesWithZero(int nums[], int n) {
+    for(int i = 0; i < n; i++) {
+        if(nums[i] < 0) {
+            nums[i] = 0;
+        }
+    }
+}
+
+void printArray(int nums[], int n) {
+    for(int i = 0; i < n; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int nums[] = {4, -3, 7, -1, 0, 5, -6};
+    int n = sizeof(nums) / sizeof(nums[0]);
+    
+    printf("Original array: ");
+    printArray(nums, n);
+
+    replaceNegativesWithZero(nums, n);
+
+    printf("Modified array: ");
+    printArray(nums, n);
+
+    return 0;
+}
