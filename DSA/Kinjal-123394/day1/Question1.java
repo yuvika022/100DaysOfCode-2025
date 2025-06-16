326e4c5839440fbb7868561c8c016746cd4ad389
Kinjal Srivastava <149761617+kinjalsri@users.noreply.github.com>
2025-06-16 18:35:13 +0530

    Update and rename DSA/Kinjal-123394/D1Q1.java to DSA/Kinjal-123394/day1/Question1.java
    Q1: remove Negteives

--- DSA/Kinjal-123394/day1/Question1.java
+++ DSA/Kinjal-123394/day1/Question1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class RemoveNegetive {
+public class Question1 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
