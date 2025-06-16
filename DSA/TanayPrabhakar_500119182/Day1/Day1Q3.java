7788e06366b941ac6a3759141989a3b82e2a35b7
Tanay <133881515+tanayprabhakar@users.noreply.github.com>
2025-06-16 22:22:14 +0530

    Update Day1Q3.java

--- DSA/TanayPrabhakar_500119182/Day1/Day1Q3.java
+++ DSA/TanayPrabhakar_500119182/Day1/Day1Q3.java
@@ -1,5 +1,5 @@
 public class Day1Q3 {
-    public int sumOfElements(int[] nums) {
+    public int sumOfElements(int nums) {
         int sum = 0;
         for (int num : nums) {
             sum += num;
