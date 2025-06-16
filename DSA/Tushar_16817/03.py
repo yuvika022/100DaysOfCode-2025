bba5e828e205b4cb05491372a4ed2c79419d7106
Tushar Mishra <tusharmishra587@gmail.com>
2025-06-16 22:55:52 +0530

    Add files via upload

---
+++ DSA/Tushar_16817/03.py
@@ -0,0 +1,7 @@
+def sum_of_array(nums):
+    total = 0
+    for num in nums:
+        total += num
+    return total
+print(sum_of_array([1, 2, 3, 4, 5]))  # Output: 15
+print(sum_of_array([10, 20, 30]))     # Output: 60
