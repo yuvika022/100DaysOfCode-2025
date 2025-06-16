6421ab4bf13e35dc46e6b06ec16d7e1fb7c2f81e
Sangam <89002045+CatoxiRetreater@users.noreply.github.com>
2025-06-16 22:52:04 +0530

    Create Day1Q3.java

---
+++ DSA/SangamKhanna_500123465/Day1Q3.java
@@ -0,0 +1,9 @@
+public class Day1Q {
+    public int sumOfElements(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+}
