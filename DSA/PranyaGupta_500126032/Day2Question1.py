c5ca35d05775263155f8ec9af98f598df9d09058
Pranya13 <pranyagupta1345@gmail.com>
2025-06-17 18:45:45 +0530

    Day 2 solutions uploaded

--- DSA/PranyaGupta_500126032/Day2Question1.py
+++ DSA/PranyaGupta_500126032/Day2Question1.py
@@ -1,16 +1,15 @@
 class Solution:
-    def SumOfArray(self, arr):
+    def SumOfEvenNum(self, arr):
         sum = 0
         for n in arr:
-            sum+=n #adding all elements to sum
-        return sum                           
-
-        
+            if n%2==0:  #check if n is even
+                sum+=n #add it to sum
+        return sum
 def main():
     user_input = input("Enter elements of the array (with space in between): ")
     arr = list(map(int, user_input.strip().split()))
     sol = Solution()
-    result = sol.SumOfArray(arr)
+    result = sol.SumOfEvenNum(arr)
     print(result)
 if __name__ == "__main__":
     main()
