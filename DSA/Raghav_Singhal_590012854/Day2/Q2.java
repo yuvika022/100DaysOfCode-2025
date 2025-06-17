00b817f4efd8bdb2fe2a92cab4fd4db4efe2de6d
Raghav <raghavsinghal638@gmail.com>
2025-06-17 18:29:31 +0530

    Day 2 Questions with '#' will be ignored, and an empty message aborts the commit.

---
+++ DSA/Raghav_Singhal_590012854/Day2/Q2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Q2 {
+    public static void main(String args[])
+    {
+        int nums[] = {1, 2, 3, 4};
+        System.out.println("First: "+nums[0]+", Last: "+nums[nums.length-1]);
+    }
+}
