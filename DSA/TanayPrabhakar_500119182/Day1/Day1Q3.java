2af09529de68066b8d870a4b479d18130cbb574a
Tanay <133881515+tanayprabhakar@users.noreply.github.com>
2025-06-18 01:28:04 +0530

    issues with the code

--- DSA/TanayPrabhakar_500119182/Day1/Day1Q3.java
+++
@@ -1,9 +0,0 @@
-public class Day1Q3 {
-    public int sumOfElements(int[] nums) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        return sum;
-    }
-}
