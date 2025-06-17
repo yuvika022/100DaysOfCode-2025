1511ea7b032f9b5447fc99e22f6107d64ccddbe7
Amrit Raj Garg <166982958+AmritRajGarg@users.noreply.github.com>
2025-06-17 23:30:43 +0530

    Update Question1.java
    Added comments

--- DSA/AmritRajGarg_500125545/Day2/Question1.java
+++ DSA/AmritRajGarg_500125545/Day2/Question1.java
@@ -1,3 +1,11 @@
+/*
+1. Sum of All Even Numbers
+A problem that combines conditional logic with accumulation to filter and sum specific elements.
+Given an array of integers nums of size n, calculate and return the sum of all even numbers in the array.
+This problem combines conditional logic with accumulation, teaching you to filter elements based on specific criteria 
+while performing mathematical operations on the filtered data.
+*/
+
 public class Question1 {
     public static void main(String[] args) {
         int[] nums = {4, 7, 2, 9, 6, 3, 8};
