ebfb1ac0c3c44703f32ba540a339386d4d7d0b8c
Hritvik Garg <149789848+hritvikgarg@users.noreply.github.com>
2025-06-18 15:38:07 +0530

    Update Q2.java

--- DSA/Hritvik_500120426/Day3/Q2.java
+++ DSA/Hritvik_500120426/Day3/Q2.java
@@ -2,5 +2,7 @@ import java.util.*;
 public class Q2{
   public static void main(string[]args){
     int nums[]={1,2,2,3,4};
-    System.out.print(contains Duplicate(nums));
+    System.out.print(containsDuplicate(nums));
   }
+public static void containsDuplicate(int nums[]){
+  
