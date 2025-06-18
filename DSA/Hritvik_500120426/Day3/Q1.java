e42fa332e78da89058fdf1e56faaca6136119592
Hritvik Garg <149789848+hritvikgarg@users.noreply.github.com>
2025-06-18 14:49:35 +0530

    Create Q1.java

---
+++ DSA/Hritvik_500120426/Day3/Q1.java
@@ -0,0 +1,7 @@
+public class question1 {
+    public static void main(String[] args) {
+        int nums[] = {1, 8, 8, 8, 5, 6, 2};
+        int res = Ksum(nums,3);
+        System.out.println(res);
+    }
+   
