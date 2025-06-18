32e5383f939d0a1dd027eec5ee33521a5f0ab139
ShatakshiMishra-02 <shatakshi2005mishra@gmail.com>
2025-06-19 00:22:21 +0530

    Update Day4Question1.cpp

--- DSA/ShatakshiMishra_590015008/Day4Question1.cpp
+++ DSA/ShatakshiMishra_590015008/Day4Question1.cpp
@@ -5,6 +5,11 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    if(n==0)
+    {
+        cout << "Array is empty" << endl;
+        return 0;
+    }
 
     vector<int> nums(n);
     for (int i = 0; i < n; ++i) {
