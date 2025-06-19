cfb9384e55123039597ee9c7c3edf5502678cafb
RonsumGameDev <ronakdwivedi69@gmail.com>
2025-06-19 22:42:11 +0530

    LogicCommentsUpdate D4P3.c

--- DSA/SakshamDwivedi_590013147/Day4/D4P3.c
+++ DSA/SakshamDwivedi_590013147/Day4/D4P3.c
@@ -1,3 +1,5 @@
+/*This is my personal favourite one. Here what happens is that the original elements of the array are replaced with the desired elements and to keep the array 
+the same way length is decreased according to the number of undesired elements [Code by Saksham Dwivedi (Comments added later)]*/
 #include <stdio.h>
 
 int arr[100];
