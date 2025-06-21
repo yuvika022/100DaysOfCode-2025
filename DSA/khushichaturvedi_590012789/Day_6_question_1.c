e7287f4f05961e0b07e99ee966ecd52eca6ce3a0
khushi chaturvedi <khushichaturvedi42006@gmail.com>
2025-06-21 16:49:24 +0530

    Create Day_6_question_1.c

---
+++ DSA/khushichaturvedi_590012789/Day_6_question_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int findDuplicate(int* nums, int size) {
+    int slow = nums[0];
+    int fast = nums[0];
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+    fast = nums[0];
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow; 
+}
+int main() {
+    int nums[] = {3, 1, 3, 4, 2}; 
+    int size = sizeof(nums) / sizeof(nums[0]);
+    int duplicate = findDuplicate(nums, size);
+    printf("Duplicate number is: %d\n", duplicate);
+    return 0;
+}
