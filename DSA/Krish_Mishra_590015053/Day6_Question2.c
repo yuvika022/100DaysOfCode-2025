ca500caf21ef12c73bc9f74c8d3cefb7b7edf5e0
KRISH MISHRA <ashamishra122098@gmail.com>
2025-06-21 14:27:32 +0530

    Update and rename D6_Q2.c to Day6_Question2.c

--- DSA/Krish_Mishra_590015053/Day6_Question2.c
+++ DSA/Krish_Mishra_590015053/Day6_Question2.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 int size;
 int  [100];
 int mon = 1;
-
 int main(){
     printf("Enter the number of elements (Less than 100) : ");
     scanf("%d", &size);
@@ -11,7 +9,6 @@ int main(){
     for(int i = 0; i < size; i++){
         scanf("%d", &arr[i]);
     }
-
     for(int i = 1; i < size; i++){
         if(arr[i] - arr[i-1] <= 0){
             mon = 0;
@@ -21,7 +18,6 @@ int main(){
             mon = 1;
         }
     }
-
     if(mon){
         printf("True");
     }
