07b912508cb604ef4602d867f1c8d5ea2409f02e
Shlok Madan <shlokmadan8@gmail.com>
2025-06-21 23:06:33 +0530

    Create day6_q2.c
    created day6_q2

---
+++ DSA/Shlok_590017851/day6_q2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool isStrictlyIncreasing(int nums[], int size)
+{
+    for (int i = 1; i < size; i++)
+      {
+        if (nums[i] <= nums[i - 1]) 
+        {
+            return false;  
+        }
+    }
+    return true; 
+}
+int main() 
+{
+    int nums[] = {1, 2, 3, 4};  
+    int size = sizeof(nums) / sizeof(nums[0]);
+    if (isStrictlyIncreasing(nums, size)) 
+    {
+        printf("The array is strictly increasing.\n");
+    } 
+    else 
+    {
+        printf("The array is NOT strictly increasing.\n");
+    }
+    return 0;
+}
