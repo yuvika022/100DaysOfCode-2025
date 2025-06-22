656ad5b204e434414205b73dd702f9dc2b05d0f2
Saksham Dwivedi <ronakdwivedi69@gmail.com>
2025-06-22 20:49:27 +0530

    Update Quiz_1_Debugging_Question_1.c

--- Quiz 1/SakshamDwivedi_590013147/Quiz_1_Debugging_Question_1.c
+++ Quiz 1/SakshamDwivedi_590013147/Quiz_1_Debugging_Question_1.c
@@ -14,6 +14,13 @@ int main(){
 //current sum will be nums[0] not 0 otherwise it will reset to 0.
 //Start loop with index 1
 //We check whether if the currentSum is less than 0, if it is then it is reset to 0.
+//The condition [if(currentSum < 0){currentSum = nums[i];}] checks for negative arrays.
+/*For ex : In the array {-3, -2, -1, -4}, current sum and max sum start from -3, then current sum becomes -2 as current sum < 0 and 
+if (currentSum > maxSum){maxSum = currentSum;} makes the max sum as -2 too, then current sum and max sum become -1, then current sum becomes -4 but max sum remians
+-1 and that's how it returns -1, and deals with all negative arrays.
+
+So this array returns -1.
+*/
 int maxSubarraySum(int nums[], int n)
 {
     int maxSum = nums[0];
