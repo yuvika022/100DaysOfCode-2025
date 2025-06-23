caec7bd39c7ad35e87767da41c1a503e1f705735
Divyansh <divyanshkumarsingh29@gmail.com>
2025-06-24 01:42:17 +0530

    Submitted By Divyansh Kumar Singh 590012175

--- DSA/DivyanshKumarSingh_590012175/Day_9_Question_1.py
+++ DSA/DivyanshKumarSingh_590012175/Day_9_Question_1.py
@@ -1,13 +1,9 @@
-def is_palindrome(s):
-    # Keep only alphabetic characters and convert to lowercase
-    filtered = ''.join(char.lower() for char in s if char.isalpha())
+def isPalindrome(s):
+    str = ''.join(char.lower() for char in s if char.isalpha())
     
-    # Check if the filtered string is equal to its reverse
-    return filtered == filtered[::-1]
+    return str == str[::-1]
 
-# Take input from the user
-user_input = input("Enter a string to check if it's a palindrome: ")
+inp = input("Enter a string to check if it's a palindrome: ")
 
-# Check and print the result
-print(is_palindrome(user_input))
+print(isPalindrome(inp))
     
