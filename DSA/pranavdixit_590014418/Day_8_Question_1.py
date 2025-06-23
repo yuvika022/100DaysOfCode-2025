8af77dcf6bfdb13d1eea664e82e264036c1e2c95
PranavDixit-25 <pranavking90@gmail.com>
2025-06-23 13:00:56 +0530

    Added Day 8 solutions inside pranavdixit_590014418 folder - Pranav Dixit

---
+++ DSA/pranavdixit_590014418/Day_8_Question_1.py
@@ -0,0 +1,10 @@
+def string_info(s):
+    length = len(s)
+    uppercase = s.upper()
+    lowercase = s.lower()
+    print(f"Length: {length}, Original: \"{s}\", Uppercase: \"{uppercase}\", Lowercase: \"{lowercase}\"")
+
+# Example usage
+string_info("Hello World")
+string_info("Python")
+string_info("pranavDixit1234")
