67c75eaa8364958666f3a4ad073d029596393375
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-06-24 00:31:36 +0530

    Update Q1.java

--- DSA/JahnaviSaxena_500119613/Day9/Q1.java
+++ DSA/JahnaviSaxena_500119613/Day9/Q1.java
@@ -1,14 +1,16 @@
 public class question1 {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("racecar"));
+        System.out.println(isPalindrome("Madam"));
     }
     public static boolean isPalindrome(String s){
+
+        s= s.toLowerCase();
         char ch[] = s.toCharArray();
         int left = 0;
         int right = ch.length - 1;
 
         while(left< right){
-            if(ch[left] != ch[right]){
+            if(ch[left]!= ch[right]){
                 return false;
             }
             left++;
