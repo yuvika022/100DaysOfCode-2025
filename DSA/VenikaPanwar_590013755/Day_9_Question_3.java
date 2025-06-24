8948ba276eea7695981957218d6bf4b78bcecc70
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-06-24 15:01:54 +0530

    Day_9_Question_3.java

--- DSA/VenikaPanwar_590013755/Day_9_Question_3.java
+++ DSA/VenikaPanwar_590013755/Day_9_Question_3.java
@@ -1,6 +1,6 @@
 import java.util.;
 
-public class FirstNonRepeatingChar {
+public class Day_9_Question_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string:");
