78e3af1190500ba280f17156e1674a5ba43a26fd
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-06-24 14:59:32 +0530

    Day_9_Question_2.java

--- DSA/VenikaPanwar_590013755/Day_9_Question_2.java
+++ DSA/VenikaPanwar_590013755/Day_9_Question_2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class VowelConsonantCounter {
+public class Day_9_Question_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string:");
