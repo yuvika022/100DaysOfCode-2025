aab637c957a7aed62dd4371c41b47129f7be3479
Divyansh <divyanshkumarsingh29@gmail.com>
2025-06-25 00:47:57 +0530

    Submitted By Divyansh Kumar Singh 590012175

--- DSA/DivyanshKumarSingh_590012175/Day_10_Question_3.py
+++ DSA/DivyanshKumarSingh_590012175/Day_10_Question_3.py
@@ -1,19 +1,15 @@
 from collections import Counter
 
-def are_anagrams(str1, str2):
-    # Remove spaces and convert to lowercase
-    cleaned1 = str1.replace(" ", "").lower()
-    cleaned2 = str2.replace(" ", "").lower()
+def isAnagram(str1, str2):
+    c1 = str1.replace(" ", "").lower()
+    c2 = str2.replace(" ", "").lower()
 
-    # Compare character frequencies
-    return Counter(cleaned1) == Counter(cleaned2)
+    return Counter(c1) == Counter(c2)
 
-# Take input from the user
 s1 = input("Enter first string: ")
 s2 = input("Enter second string: ")
 
-# Check and print result
-if are_anagrams(s1, s2):
+if aisAnagram(s1, s2):
     print("true")
 else:
     print("false")
