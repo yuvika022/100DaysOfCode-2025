772c1f3ada1f496318ed4dec38b2117ac739a139
Samagra Gupta <gsamagra93@gmail.com>
2025-06-24 16:52:35 +0530

    Day 9 Question 2

--- DSA/SAMAGRA_590014891/Day_9_Question_2.c
+++ DSA/SAMAGRA_590014891/Day_9_Question_2.c
@@ -8,7 +8,7 @@ int main() {
 
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
-    
+
     int len = strlen(str);
     if (len > 0 && str[len - 1] == '\n') {
         str[len - 1] = '\0';
