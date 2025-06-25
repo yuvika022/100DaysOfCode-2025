b0d51d97082799ae2f2178b456bd35b27c505a1a
KRISH MISHRA <ashamishra122098@gmail.com>
2025-06-25 22:03:35 +0530

    Update and rename Day10_Question3.c to Day10_Question_3.c

--- DSA/Krish_Mishra_590015053/Day10_Question_3.c
+++ DSA/Krish_Mishra_590015053/Day10_Question_3.c
@@ -10,17 +10,13 @@ void cleanString(char *str, char *cleaned) {
     }
     cleaned[j] = '\0';
 }
-
 int areAnagrams(char *str1, char *str2) {
     int count1[26] = {0}, count2[26] = {0};
     int i;
-
     for (i = 0; str1[i] != '\0'; i++)
         count1[str1[i] - 'a']++;
-
     for (i = 0; str2[i] != '\0'; i++)
         count2[str2[i] - 'a']++;
-
     for (i = 0; i < 26; i++) {
         if (count1[i] != count2[i])
             return 0;
