69564b9ade4af99702ba6a06ffbd4f8355012fc5
Hritvik Garg <149789848+hritvikgarg@users.noreply.github.com>
2025-06-25 21:27:54 +0530

    Update Q2.java

--- DSA/Hritvik_500120426/Day10/Q2.java
+++ DSA/Hritvik_500120426/Day10/Q2.java
@@ -4,14 +4,8 @@ public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-
-        // Remove all spaces
         String removeAll = input.replace(" ", "");
-
-        // Trim leading and trailing spaces
         String trim = input.trim();
-
-        // Remove extra spaces between words (single spaces only)
         String singleSpaces = input.trim().replaceAll("\\s+", " ");
 
         System.out.println("Remove all: \"" + removeAll + "\"");
