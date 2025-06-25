97d7ebce04a9ffc9eebbf4c9b22bc47b9dbf3a1e
Hritvik Garg <149789848+hritvikgarg@users.noreply.github.com>
2025-06-25 21:25:43 +0530

    Update Q1.java

--- DSA/Hritvik_500120426/Day10/Q1.java
+++ DSA/Hritvik_500120426/Day10/Q1.java
@@ -1,26 +1,20 @@
 import java.util.Scanner;
 
-public class StringComparison {
+public class Q1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str1 = sc.nextLine();
         String str2 = sc.nextLine();
-
-        // Exact equality
         if (str1.equals(str2)) {
             System.out.print("Equal, ");
         } else {
             System.out.print("Not equal, ");
         }
-
-        // Case-insensitive equality
         if (str1.equalsIgnoreCase(str2)) {
             System.out.print("Equal ignoring case, ");
         } else {
             System.out.print("Not equal ignoring case, ");
         }
-
-        // Lexicographical order
         int cmp = str1.compareTo(str2);
         if (cmp == 0) {
             System.out.println("Strings are identical");
