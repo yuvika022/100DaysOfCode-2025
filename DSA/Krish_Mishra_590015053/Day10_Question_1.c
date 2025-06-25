bfa117771c1be0350531f64f42868ffb38ae5556
KRISH MISHRA <ashamishra122098@gmail.com>
2025-06-25 21:56:19 +0530

    Update and rename Day10_Question1.c to Day10_Question_1.c

--- DSA/Krish_Mishra_590015053/Day10_Question_1.c
+++ DSA/Krish_Mishra_590015053/Day10_Question_1.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
 int main() {
     char str1[100], str2[100];
     int i, flag = 1;
-
     printf("Enter String 1: ");
     fgets(str1, sizeof(str1), stdin);
     printf("Enter String 2: ");
     fgets(str2, sizeof(str2), stdin);
-
     if (str1[strlen(str1) - 1] == '\n')
         str1[strlen(str1) - 1] = '\0';
     if (str2[strlen(str2) - 1] == '\n')
         str2[strlen(str2) - 1] = '\0';
-
     if (strcmp(str1, str2) == 0)
         printf("Equal, ");
     else
         printf("Not equal, ");
-
     for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++) {
         if (tolower(str1[i]) != tolower(str2[i])) {
             flag = 0;
