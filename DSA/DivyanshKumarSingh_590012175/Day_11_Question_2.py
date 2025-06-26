feb1bd1cd2e0a3cdce7082699aa91fba49d9da27
Divyansh <divyanshkumarsingh29@gmail.com>
2025-06-26 11:15:10 +0530

    Submitted By Divyansh Kumar Singh 590012175

--- DSA/DivyanshKumarSingh_590012175/Day_11_Question_2.py
+++ DSA/DivyanshKumarSingh_590012175/Day_11_Question_2.py
@@ -3,30 +3,24 @@ def longestPalindrome(string):
     if n == 0:
         return ""
     
-    # Create a DP table to store palindrome status
     dp = [[False] * n for _ in range(n)]
 
-    start = 0 # Start index of the longest palindromic substring
-    mlen = 1 # At least one character is a palindrome
+    start = 0 
+    mlen = 1 
 
-    # Every single character is a palindrome
-    # All substrings of length 1 are palindromes
     for i in range(n):
         dp[i][i] = True
 
-    # Check substrings of length 2
     for i in range(n - 1):
         if string[i] == string[i + 1]:
             dp[i][i + 1] = True
             start = i
             mlen = 2
 
-    # Check substrings of length 3 or more
     for length in range(3, n + 1):
         for i in range(n - length + 1):
-            j = i + length - 1 # Ending index
+            j = i + length - 1 
 
-            # Check if inner substring is a palindrome and outer characters match
             if string[i] == string[j] and dp[i + 1][j - 1]:
                 dp[i][j] = True
                 start = i
@@ -34,9 +28,7 @@ def longestPalindrome(string):
 
     return string[start:start + mlen]
 
-# Take input and run
 inp = input("Enter a string: ")
 
-# Find and print the longest palindromic substring
 res = longestPalindrome(inp)
 print("Longest Palindromic substring:", res)
