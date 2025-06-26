f74ccff5ed3ffc7e8ca2feb28a4fb002e8881781
Divyansh <divyanshkumarsingh29@gmail.com>
2025-06-26 11:14:07 +0530

    Submitted By Divyansh Kumar Singh 590012175

--- DSA/DivyanshKumarSingh_590012175/Day_11_Question_1.py
+++ DSA/DivyanshKumarSingh_590012175/Day_11_Question_1.py
@@ -1,14 +1,10 @@
 def isPalindrome(str):
-    # Keep only alphanumeric characters and convert to lowercase
     s=''.join(char.lower() for char in str if char.isalnum())
 
-    # Check if cleaned string is equal to its reverse
     return s==s[::-1]
 
-#Take user input
 inp=input("Enter a string: ")
 
-#Check and print result
 if (isPalindrome(inp)):
     print("true")
 else:
