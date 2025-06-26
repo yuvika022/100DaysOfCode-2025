34969b957ddf7395ccb6b8ed440823a01691a695
Gehna-01 <gehnakhanna2005@gmail.com>
2025-06-26 22:38:05 +0530

    Create day8question2.c

---
+++ DSA/GEHNA_KHANNA_590013140/day8question2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+    char str[100];
+    char ch;
+    int count = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    printf("Enter the character to count: ");
+    scanf("%c", &ch);
+    for (int i = 0; str[i] != '\0'; i++) 
+    {
+        if (str[i] == ch) 
+        {
+            count++;
+        }
+    }
+    printf("The character '%c' occurs %d times in the string.\n", ch, count);
+    printf("\n");
+    return 0;
+}
+Footer
+© 20
