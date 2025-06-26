7fe3974e72b90c74f86f70b7b0bf847380e371e9
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-06-26 18:04:35 +0530

    Day_11_Question_3.java

--- DSA/VenikaPanwar_590013755/Day_11_Question_3.java
+++ DSA/VenikaPanwar_590013755/Day_11_Question_3.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class CountPalindromicSubstrings {
+public class Day_11_Question_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string:");
