a7362cfaef4f265756b38efc7b25dfc1db48853b
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-06-26 17:52:23 +0530

    Day_11_Question_1.java

--- DSA/VenikaPanwar_590013755/Day_11_Question_1.java
+++ DSA/VenikaPanwar_590013755/Day_11_Question_1.java
@@ -1,6 +1,6 @@
-import java.util.;
+import java.util.*;
 
-public class AdvancedPalindrome {
+public class Day_11_Question_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string:");
