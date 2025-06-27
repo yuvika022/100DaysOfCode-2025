fa08e373df4525690141c9638f0e644a3b8cb7a5
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-06-27 14:21:54 +0530

    Day_12_Question_3.java

--- DSA/VenikaPanwar_590013755/Day_12_Question_3.java
+++ DSA/VenikaPanwar_590013755/Day_12_Question_3.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class StringCompressor {
+public class Day_12_Question_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string:");
