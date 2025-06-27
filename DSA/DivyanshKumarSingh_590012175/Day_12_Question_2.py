199ae9c7fb74fe3b75c0e1351a0a18dade242940
Divyansh <divyanshkumarsingh29@gmail.com>
2025-06-27 22:15:31 +0530

    Submitted By Divyansh Kumar Singh 590012175

--- DSA/DivyanshKumarSingh_590012175/Day_12_Question_2.py
+++ DSA/DivyanshKumarSingh_590012175/Day_12_Question_2.py
@@ -1,23 +1,17 @@
 def remDuplicate(s):
-    seen = set() # Set to keep track of characters already encountered
-    res = []     # List to store result with unique characters
+    seen = set() 
+    res = []
 
-     # Traverse each character in the input string
     for char in s:
-        # If the character hasn't been seen before, add to result
         if char not in seen:
-            seen.add(char)     # Mark character as seen
-            res.append(char)   # Add character to the result list
+            seen.add(char)     
+            res.append(char)   
 
-    # Join the list into a final string and return
     return ''.join(res)
 
 
-# Take input from the user
 inp = input("Enter a string: ")
 
-# Remove duplicate characters
 res = remDuplicate(inp)
 
-# Display the result
 print("String after removing Duplicate characters:", res)
