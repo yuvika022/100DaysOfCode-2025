de37e96fa526ca34434fa2e18508ebb26bedcabd
KRISH MISHRA <ashamishra122098@gmail.com>
2025-06-27 15:53:02 +0530

    Create Day12_Question2.c

---
+++ DSA/Krish_Mishra_590015053/Day12_Question2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+void removeDuplicates(char* str) {
+    bool seen[256] = { false };
+    int index = 0;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!seen[(unsigned char)str[i]]) {
+            seen[(unsigned char)str[i]] = true;
+            str[index++] = str[i];
+        }
+    }
+    str[index] = '\0';
+    printf("Output: %s\n", str);
+}
+
+int main() {
+    char input[1000];
+    printf("Enter the string: ");
+    scanf("%s", input);
+    removeDuplicates(input);
+    return 0;
+}
