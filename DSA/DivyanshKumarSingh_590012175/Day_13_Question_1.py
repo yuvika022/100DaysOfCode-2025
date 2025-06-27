a4e654331f0bc02b74bee2aee32f080105df0166
Divyansh <divyanshkumarsingh29@gmail.com>
2025-06-28 01:29:04 +0530

    Update Day_13_Question_1.py

--- DSA/DivyanshKumarSingh_590012175/Day_13_Question_1.py
+++ DSA/DivyanshKumarSingh_590012175/Day_13_Question_1.py
@@ -9,12 +9,10 @@ def countWords(s):
     longestWord = ''
 
     for w in words:
-        # If current word's length is greater than max so far, update it
         if len(w) > m1:
             m1 = len(w)
             longestWord = w
 
-    # Return the word count, the longest word, and its length
     return len(words), longestWord, m1
 
 # Take input from user and strip any outer double quotes if entered
