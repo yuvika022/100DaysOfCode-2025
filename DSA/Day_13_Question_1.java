9954524f2227d0b41b6d2aa073bf560ce32dd45f
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-06-28 14:13:57 +0530

    Day_13_Question_1.java

---
+++ DSA/Day_13_Question_1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String line = sc.nextLine();
+
+        // Trim leading/trailing and normalize spacing
+        line = line.trim();
+        String word = "";
+        int count = 0;
+        String longest = "";
+
+        for (int i = 0; i <= line.length(); i++) {
+            char ch = (i < line.length()) ? line.charAt(i) : ' ';
+            if (ch != ' ') {
+                word += ch;
+            } else if (!word.equals("")) {
+                count++;
+                if (word.length() > longest.length()) {
+                    longest = word;
+                }
+                word = "";
+            }
+        }
+
+        System.out.println("Word count: " + count);
+        System.out.println("Longest word: \"" + longest + "\"");
+    }
+}
