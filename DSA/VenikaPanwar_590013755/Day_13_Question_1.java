a4d87da0e39b4b23f5a480e75da92b56003c7cc7
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-06-28 14:19:20 +0530

    Day_13_Question_1.java

--- DSA/VenikaPanwar_590013755/Day_13_Question_1.java
+++ DSA/VenikaPanwar_590013755/Day_13_Question_1.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class WordCount {
+public class Day_13_Question_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a sentence:");
