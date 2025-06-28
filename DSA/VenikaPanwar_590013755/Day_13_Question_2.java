8977e525a235880a3249fa3a5a0443ba7827e4d7
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-06-28 14:26:11 +0530

    Day_13_Question_2.java

--- DSA/VenikaPanwar_590013755/Day_13_Question_2.java
+++ DSA/VenikaPanwar_590013755/Day_13_Question_2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class PalindromePermutation {
+public class Day_13_Question_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string:");
