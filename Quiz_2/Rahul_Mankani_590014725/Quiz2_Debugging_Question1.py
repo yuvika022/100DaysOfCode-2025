528d1f064c352358270f0ca4af8ecf13693cdb22
Rahul Mankani <vishalranka763@gmail.com>
2025-06-29 20:27:34 +0530

    Quiz2 Debugging Question1 - Rahul Mankani 590014725

--- Quiz_2/Rahul_Mankani_590014725/Quiz2_Debugging_Question1.py
+++ Quiz_2/Rahul_Mankani_590014725/Quiz2_Debugging_Question1.py
@@ -1,6 +1,6 @@
 def is_digits_only(s):
     if len(s) == 0:
-        return False //Issue was: (A) Empty string should return False since it contains no digits
+        return False #Issue was: (A) Empty string should return False since it contains no digits
     for char in s:
         if char < '0' or char > '9':
             return False
