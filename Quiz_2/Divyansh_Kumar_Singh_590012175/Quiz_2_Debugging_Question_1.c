77cf1cf983fb361612eea691fd3fcd35e5cd60aa
Divyansh <divyanshkumarsingh29@gmail.com>
2025-06-29 20:51:48 +0530

    Submitted By Divyansh Kumar Singh 590012175

--- Quiz_2/Divyansh_Kumar_Singh_590012175/Quiz_2_Debugging_Question_1.c
+++ Quiz_2/Divyansh_Kumar_Singh_590012175/Quiz_2_Debugging_Question_1.c
@@ -1,1 +1,15 @@
 # Divyansh Kumar Singh 590012175
+
+def is_digits_only(s):
+    # Return False for empty string since it contains no digits
+    if not s:
+        return False
+
+    for char in s:
+        if char < '0' or char > '9':
+            return False
+
+    return True
+
+# Explanation:
+# A) Empty string should return False since it contains no digits
