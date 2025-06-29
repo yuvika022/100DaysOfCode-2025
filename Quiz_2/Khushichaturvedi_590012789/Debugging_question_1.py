72398e01c115c8b49ef8040cc9c564bd579ec1f3
khushi chaturvedi <khushichaturvedi42006@gmail.com>
2025-06-29 22:45:38 +0530

    Create Debugging_question_1.py

---
+++ Quiz_2/Khushichaturvedi_590012789/Debugging_question_1.py
@@ -0,0 +1,10 @@
+#the correct answer is:
+ A) Empty string should return False since it contains no digits
+
+def is_digits_only(s):
+    if len(s) == 0:
+        return False  # Empty string contains no digits
+    for char in s:
+        if char < '0' or char > '9':
+            return False
+    return True
