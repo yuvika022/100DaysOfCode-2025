2a0584ba2a3bbd0e7cbdb3447e5bd562a68693a4
Diptansh <diptanshnagar@gmail.com>
2025-06-29 20:49:45 +0530

    Update Quiz_2_Debugging_Question_1.py

--- Quiz_2/Deeptansh_Nagar_500121033/Quiz_2_Debugging_Question_1.py
+++ Quiz_2/Deeptansh_Nagar_500121033/Quiz_2_Debugging_Question_1.py
@@ -1,10 +1,10 @@
-// Bug in this code -
-//  function returns True when the input string is empty (""), which is not logically correct. // empty string doesnt contain any digits so it should return false
-// if len(s) == 0:
-//       return True   // incorrect logic // it should return false.
+# Bug in this code -
+#  function returns True when the input string is empty (""), which is not logically correct. # empty string doesnt contain any digits so it should return false
+# if len(s) == 0:
+#       return True   # incorrect logic      # it should return false.
 
-// correct option - A) Empty string should return False since it contains no digits
-// correct code - 
+# correct option - A) Empty string should return False since it contains no digits
+# correct code - 
 def check_digits(s):
     if len(s) == 0:
         return False  # issue fixed ;o
@@ -13,7 +13,7 @@ def check_digits(s):
             return False
     return True
 
-# Test cases
+# all Test case
 test_cases = ["123", "", "12a3", "007", "abc"]
 for test in test_cases:
     result = check_digits(test)
