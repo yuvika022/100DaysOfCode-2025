6a33807e35d977311919962e946ca87a0894e99c
Rahul Mankani <vishalranka763@gmail.com>
2025-06-29 20:28:31 +0530

    Quiz2 Debugging Question2 - Rahul Mankani 590014725

--- Quiz_2/Rahul_Mankani_590014725/Quiz2_Debugging_Question2.c
+++
@@ -1,8 +0,0 @@
-def first_non_repeating(s):
-    char_count = {}
-    for char in s:
-        char_count[char] = char_count.get(char, 0) + 1
-    for index, char in enumerate(s)://Bug: Iterating over char_count instead of s causes incorrect order and indexing.
-        if char_count[char] == 1://Why it was wrong: It wassn't preserving the original order of characters in s
-            return index  
-    return -1  
