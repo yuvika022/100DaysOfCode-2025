0fa18d35ad3755673bf526ab7f312f7f4947bdc1
Navneet Kumar <kumarnavneet7765@gmail.com>
2025-06-30 22:03:30 +0530

    Delete DSA/ManyaVats_590013483/day12_ques3.py

--- DSA/ManyaVats_590013483/day12_ques3.py
+++
@@ -1,9 +0,0 @@
-arr = [5, 2, 8, 1, 9]
-n = len(arr)
-
-for i in range(n):
-    for j in range(0, n - i - 1):
-        if arr[j] > arr[j + 1]:
-            arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-print(arr)
