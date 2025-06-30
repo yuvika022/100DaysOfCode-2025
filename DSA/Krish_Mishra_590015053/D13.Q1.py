33a0659784cbe1c07cbb1c554a38ea174e2d8c08
KRISH MISHRA <ashamishra122098@gmail.com>
2025-06-30 16:36:12 +0530

    Create D13.Q1.py

---
+++ DSA/Krish_Mishra_590015053/D13.Q1.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+def print_array(arr):
+    for num in arr:
+        print(num, end=" ")
+    print()
