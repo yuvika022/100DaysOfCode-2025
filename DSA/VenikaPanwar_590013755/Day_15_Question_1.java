94acfe950f27215731abb5d68a931164d766193d
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-06-30 13:10:20 +0530

    Day_15_Question_1.java

---
+++ DSA/VenikaPanwar_590013755/Day_15_Question_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SimpleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of elements:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr); // Built-in sort
+
+        System.out.print("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
