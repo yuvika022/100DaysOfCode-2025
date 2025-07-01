bb8f20ca093b7818b73d5ac4f75ffa9269830e14
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-07-01 14:00:25 +0530

    Day_16_Question_2.java

---
+++ DSA/VenikaPanwar_590013755/Day_16_Question_2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class CountOccurrences {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of elements:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the target value to count:");
+        int target = sc.nextInt();
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == target) {
+                count++;
+            }
+        }
+
+        System.out.println("Output: " + count);
+    }
+}
