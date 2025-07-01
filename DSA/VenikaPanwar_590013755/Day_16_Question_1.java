ce870b34ff429f53b3a450c49514746cf30e4af6
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-07-01 13:58:23 +0530

    Day_16_Question_1.java

--- DSA/VenikaPanwar_590013755/Day_16_Question_1.java
+++ DSA/VenikaPanwar_590013755/Day_16_Question_1.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class LinearSearch {
+public class Day_16_Question_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
