91be5ed481d976ea384a6a87172b164119a7b74c
Nakul Yadav <nakulydv71@gmail.com>
2025-07-01 22:08:56 +0530

    Update Q3.java

--- DSA/NakulYadav_500121882/Day16/Q3.java
+++ DSA/NakulYadav_500121882/Day16/Q3.java
@@ -1,9 +1,9 @@
 public class Q3 {
     public static void main(String[] args) {
         int num[] = {-5, -2, -8, -1, -3};
-        System.out.println(maxElement(num));
+        System.out.println(maxEl(num));
     }
-    public static int maxElement(int nums[]){
+    public static int maxEl(int nums[]){
         int max = Integer.MIN_VALUE;
         for(int  i : nums){
             if(i > max){
