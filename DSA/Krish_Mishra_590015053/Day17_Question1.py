0a0e824210442b3ec626a1ad19335a5afb7c47f7
KRISH MISHRA <ashamishra122098@gmail.com>
2025-07-02 23:00:11 +0530

    Create Day17_Question1.py

---
+++ DSA/Krish_Mishra_590015053/Day17_Question1.py
@@ -0,0 +1,19 @@
+def binary_search_user_defined():
+    arr = list(map(int, input("Enter the sorted array elements (space-separated): ").split()))
+    
+    target = int(input("Enter the target element to search: "))
+    left = 0
+    right = len(arr) - 1
+  
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if arr[mid] == target:
+            print(f"Element found at index {mid}")
+            return
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    print("Element not found (Output: -1)")
