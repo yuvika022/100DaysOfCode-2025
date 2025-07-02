b533a266e3fcfd51bdac1d11f1a5685c1aaf3ac6
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2025-07-02 08:52:10 +0530

    Day 17

---
+++ DSA/BhavyaTalwar_500121992/Day17/Q2.java
@@ -0,0 +1,32 @@
+
+public class Q2 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 2, 3, 3};
+        int num = 1;
+
+        System.out.println(first(arr, num));
+    }
+
+    public static int first(int[] arr, int num) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == num) {
+                result = mid;
+                right = mid - 1;
+            } else if (arr[mid] < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+}
