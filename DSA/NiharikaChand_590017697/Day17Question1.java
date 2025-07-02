f98cfaadc3c4b35c862482a385d6e481ba70b198
niharika24chand <nhrkchnd@gmail.com>
2025-07-02 21:53:20 +0530

    Day 17 Question 1, 2 and 3 added

---
+++ DSA/NiharikaChand_590017697/Day17Question1.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Day17Question1 
+{
+    public static int binarySearch(int arr[], int n, int item)
+    {
+        int b = 0, e = n-1, mid, p =-1;
+        while(e >= b)
+        {
+            mid = (b+e)/2;
+            if(arr[mid] == item)
+            {
+                p = mid;
+                break;
+            }
+            else if(arr[mid] < item)
+            b = mid + 1;
+            else
+            e = mid - 1;
+        }
+        return p;
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n]; int i;
+        for(i=0; i<n; i++)
+        {
+            System.out.println("Enter an element: ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter target element: ");
+        int tar = sc.nextInt();
+        int result = binarySearch(arr, n, tar);
+        if(result != -1)
+        System.out.println("Output: " + result);
+        else
+        System.out.println("Output: -1");
+    }
+}
