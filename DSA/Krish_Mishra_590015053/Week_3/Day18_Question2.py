d961273fde07de9098121e5b35fa83c8d6795c83
KRISH MISHRA <ashamishra122098@gmail.com>
2025-07-03 23:15:28 +0530

    Update and rename DAY18_QUESTION2.py to Day18_Question2.py

--- DSA/Krish_Mishra_590015053/Week_3/Day18_Question2.py
+++ DSA/Krish_Mishra_590015053/Week_3/Day18_Question2.py
@@ -1,20 +1,14 @@
 def insertion_sort(arr):
     for i in range(1, len(arr)):
-        key = arr[i]  # The element we want to insert
+        key = arr[i] 
         j = i - 1
-        # Move elements of arr[0..i-1], that are greater than key, to one position ahead
         while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]  # Shift right
+            arr[j + 1] = arr[j] 
             j -= 1
-        arr[j + 1] = key  # Insert key at correct location
+        arr[j + 1] = key 
     return arr
 
-# Taking user input
 user_input = input("Enter the numbers separated by spaces: ")
 arr = list(map(int, user_input.split()))
-
-# Sorting
 sorted_arr = insertion_sort(arr)
-
-# Output
 print("Sorted array:", sorted_arr)
