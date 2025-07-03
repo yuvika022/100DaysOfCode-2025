b4444c548f998ee41b30cc67aa9df1759b03bc26
Stuti Jain <stuti18022006@gmail.com>
2025-07-03 23:40:46 +0530

    Update Day_18_Question_1.c

--- DSA/Stuti_Jain_590012149/Day_18_Question_1.c
+++ DSA/Stuti_Jain_590012149/Day_18_Question_1.c
@@ -1,35 +1,39 @@
 #include <stdio.h>
 void ssort(int arr[], int n){
-    for(int i=0; i<n-1; i++){
-        int min=i;
-        for(int j=i+1; j<n; j++){
-            if(arr[j]<arr[min]){
-                min=j;
+    for(int i = 0; i < n - 1; i++){
+        int min = i;
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] < arr[min]){
+                min = j;
             }
         }
-        int temp=arr[i];
-        arr[i]=arr[min];
-        arr[min]=temp;
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
     }
 }
 
 void output(int arr[], int n){
-    for(int i=0; i<n; i++){
+    for(int i = 0; i < n; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
 int main(){
-    int arr[]={64,25,12,22,11};
-    int n=sizeof(arr)/sizeof(arr[0]);
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
 
-    printf("Original array: ");
-    output(arr, n);
+    int arr[n];
+    printf("Enter the elements: ", n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
 
     ssort(arr, n);
- 
-    printf("Sorted array: ");
+
+    printf("");
     output(arr, n);
 
     return 0;
