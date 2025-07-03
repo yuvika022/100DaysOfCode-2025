dd036fd163652543d5c02f78a8c07b6e3a1287a4
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-07-03 19:40:01 +0530

    Create Q3.java

---
+++ DSA/JahnaviSaxena_500119613/Day18/Q3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class question3 {
+    public static void main(String[] args) {
+        Integer arr[] = {2, 0, 2, 1, 1, 0};
+        Arrays.sort(arr);
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+
+    }
+}
