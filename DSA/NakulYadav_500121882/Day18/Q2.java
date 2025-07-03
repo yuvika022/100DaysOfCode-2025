9ffd49f6afb4796d4e92bea50cf579bf6e0f6194
Nakul Yadav <nakulydv71@gmail.com>
2025-07-03 17:42:12 +0530

    Update Q2.java

--- DSA/NakulYadav_500121882/Day18/Q2.java
+++ DSA/NakulYadav_500121882/Day18/Q2.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Q2 {
     public static void main(String[] args) {
-        int[] arr = {12, 11, 13, 5, 6};  // Example array
+        int[] arr = {12, 11, 13, 5, 6};
         insertionSort(arr);
         System.out.println("Insertion Sorted Array: " + Arrays.toString(arr));
     }
