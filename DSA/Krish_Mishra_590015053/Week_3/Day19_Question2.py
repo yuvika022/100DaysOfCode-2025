70c87fe73d003fa95f92b11ac69bcd66fb2e2d47
KRISH MISHRA <ashamishra122098@gmail.com>
2025-07-04 23:48:05 +0530

    Update and rename DAY19_Q2.py to Day19_Question2.py

--- DSA/Krish_Mishra_590015053/Week_3/Day19_Question2.py
+++ DSA/Krish_Mishra_590015053/Week_3/Day19_Question2.py
@@ -5,21 +5,13 @@ def find_peak(arr):
     while start <= end:
         mid = (start + end) // 2
 
-        # Left neighbor (handle edge)
         left = arr[mid - 1] if mid > 0 else float('-inf')
-        # Right neighbor (handle edge)
         right = arr[mid + 1] if mid < len(arr) - 1 else float('-inf')
-
-        # If mid is peak
         if arr[mid] >= left and arr[mid] >= right:
             return mid
-
-        # If left is greater, peak must be on the left side
         elif left > arr[mid]:
             end = mid - 1
-
-        # If right is greater, peak must be on the right side
         else:
             start = mid + 1
 
-    return -1  # Just in case, though problem guarantees a peak exists
+    return -1 
