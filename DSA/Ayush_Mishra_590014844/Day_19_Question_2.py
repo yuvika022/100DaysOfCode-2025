8bac53bdbfb12264d087c7e782d25c1310ba8814
Ayush Mishra <ayushmishra1699417@gmail.com>
2025-07-04 22:16:20 +0530

    Update Day_19_Question_2.py

--- DSA/Ayush_Mishra_590014844/Day_19_Question_2.py
+++ DSA/Ayush_Mishra_590014844/Day_19_Question_2.py
@@ -1,1 +1,12 @@
+def find_peak(arr):
+    low, high = 0, len(arr) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if arr[mid] < arr[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+    return low
 
+arr = list(map(int, input("Enter array: ").split()))
+print("Peak index:", find_peak(arr))
