e4f969436d744438f945e6d611a2166a4b9074a1
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-07-04 12:30:43 +0530

    Day_19_Question_1.java

---
+++ DSA/VenikaPanwar_590013755/Day_19_Question_1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class RotatedArraySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter rotated sorted array elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        System.out.println("Enter target to search:");
+        int target = sc.nextInt();
+
+        int result = search(arr, target);
+        System.out.println("Output: " + result);
+    }
+
+    public static int search(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) return mid;
+
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target < arr[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            } else {
+                if (arr[mid] < target && target <= arr[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
