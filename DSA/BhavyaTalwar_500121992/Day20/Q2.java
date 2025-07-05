6fce81b95f82b9a2adf43f2b590eecdadfc2158d
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2025-07-05 13:25:29 +0530

    Day 20

--- DSA/BhavyaTalwar_500121992/Day20/Q2.java
+++ DSA/BhavyaTalwar_500121992/Day20/Q2.java
@@ -3,40 +3,36 @@ import java.util.*;
 
 class Q2 {
 
-    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 5, 9, 10};
+        int[] arr2 = {2, 3, 8, 13};
+
+        int[] merged = merge(arr1, arr2);
+        System.out.println(Arrays.toString(merged));
+    }
+
+    public static int[] merge(int[] arr1, int[] arr2) {
         int n = arr1.length, m = arr2.length;
-        int[] result = new int[n + m];
+        int[] res = new int[n + m];
 
         int i = 0, j = 0, k = 0;
-
-        // Merge while both arrays have elements
         while (i < n && j < m) {
             if (arr1[i] <= arr2[j]) {
-                result[k++] = arr1[i++];
+                res[k++] = arr1[i++];
             } else {
-                result[k++] = arr2[j++];
+                res[k++] = arr2[j++];
             }
         }
 
-        // Copy remaining elements from arr1 (if any)
         while (i < n) {
-            result[k++] = arr1[i++];
+            res[k++] = arr1[i++];
         }
 
-        // Copy remaining elements from arr2 (if any)
         while (j < m) {
-            result[k++] = arr2[j++];
+            res[k++] = arr2[j++];
         }
 
-        return result;
+        return res;
     }
 
-    public static void main(String[] args) {
-        int[] arr1 = {1, 3, 5, 7};
-        int[] arr2 = {2, 4, 6, 8};
-
-        int[] merged = mergeSortedArrays(arr1, arr2);
-
-        System.out.println("Merged Array: " + Arrays.toString(merged));
-    }
 }
