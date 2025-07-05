acbb3db7a5164324b52ddeb9594fdb80f3586d3b
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-07-05 12:29:43 +0530

    Day_20_Question_2.java

--- DSA/VenikaPanwar_590013755/Day_20_Question_2.java
+++ DSA/VenikaPanwar_590013755/Day_20_Question_2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class MergeSortedArrays {
+public class Day_20_Question_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
