296a953f6d1ec4ff04bae1f227249e6c142e7a25
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-07-05 12:28:17 +0530

    Day_20_Question_1.java

--- DSA/VenikaPanwar_590013755/Day_20_Question_1.java
+++ DSA/VenikaPanwar_590013755/Day_20_Question_1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class SortStudentsByGrade {
+public class Day_20_Question_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
