818e2dc58d1514de69536ff68750098d9c809b2c
Samagra Gupta <gsamagra93@gmail.com>
2025-07-05 21:39:10 +0530

    Day 20 Question 2

---
+++ DSA/SAMAGRA_590014891/Day_20_Question_2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void mergeSortedArrays(int a[], int n, int b[], int m, int result[]) {
+    int i = 0, j = 0, k = 0;
+
+    // Merge elements until one array is fully traversed
+    while (i < n && j < m) {
+        if (a[i] < b[j]) {
+            result[k++] = a[i++];
+        } else {
+            result[k++] = b[j++];
+        }
+    }
+
+    // Copy remaining elements of a[]
+    while (i < n) {
+        result[k++] = a[i++];
+    }
+
+    // Copy remaining elements of b[]
+    while (j < m) {
+        result[k++] = b[j++];
+    }
+}
+
+void printArray(int arr[], int size) {
+    printf("[");
+    for (int i = 0; i < size; i++) {
+        printf("%d", arr[i]);
+        if (i != size - 1) printf(", ");
+    }
+    printf("]\n");
+}
+
+int main() {
+    int a[] = {10, 30, 50};
+    int b[] = {20, 40, 60, 70};
+
+    int n = sizeof(a) / sizeof(a[0]);
+    int m = sizeof(b) / sizeof(b[0]);
+    int result[n + m];
+
+    mergeSortedArrays(a, n, b, m, result);
+    printArray(result, n + m);
+
+    return 0;
+}
