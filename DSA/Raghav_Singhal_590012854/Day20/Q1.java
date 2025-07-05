b60e55f6fbce5fe6f14075a081bb46babf34433d
Raghav <raghavsinghal638@gmail.com>
2025-07-05 14:54:59 +0530

    Day20 Questions

---
+++ DSA/Raghav_Singhal_590012854/Day20/Q1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+// Step 1: Define a Student class
+class Student {
+    String name;
+    int grade;
+    Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+    @Override public String toString() {
+        return "(\"" + name + "\", " + grade + ")";
+    }
+}
+public class Q1
+{
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Ram", 85));
+        students.add(new Student("Bhuvi", 92));
+        students.add(new Student("Chetan", 78));
+
+        for (int i = 0; i < students.size() - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < students.size(); j++) {
+                if (students.get(j).grade > students.get(minIdx).grade) {
+                    minIdx = j;
+                }
+            }
+            Student temp = students.get(i);
+            students.set(i, students.get(minIdx));
+            students.set(minIdx, temp);
+        }
+        System.out.println(students.toString());
+    }
+}
