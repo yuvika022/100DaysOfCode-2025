a5770a7dc73701a90d39c77b04e67e0eef844f82
Venika Panwar <vainikapanwarr.21042@gmail.com>
2025-07-05 12:31:36 +0530

    Day_20_Question_3.java

--- DSA/VenikaPanwar_590013755/Day_20_Question_3.java
+++ DSA/VenikaPanwar_590013755/Day_20_Question_3.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class KthLargestElement {
+public class Day_20_Question_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
