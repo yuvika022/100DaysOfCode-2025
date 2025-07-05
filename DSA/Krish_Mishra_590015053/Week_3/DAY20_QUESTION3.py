2492cd355473d0058466d1096dd3016d3af6c23a
KRISH MISHRA <ashamishra122098@gmail.com>
2025-07-05 23:56:12 +0530

    Create DAY20_QUESTION3.py

---
+++ DSA/Krish_Mishra_590015053/Week_3/DAY20_QUESTION3.py
@@ -0,0 +1,18 @@
+import heapq
+
+def find_kth_largest(arr, k):
+    min_heap = arr[:k]
+    heapq.heapify(min_heap)
+
+    for num in arr[k:]:
+        if num > min_heap[0]:
+            heapq.heappushpop(min_heap, num)
+
+    return min_heap[0]
+
+# User input
+arr = list(map(int, input("Enter the array elements (space-separated): ").split()))
+k = int(input("Enter the value of K: "))
+
+result = find_kth_largest(arr, k)
+print(f"{k}th Largest Element:", result)
