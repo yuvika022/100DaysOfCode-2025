615503f8e5b5e6ae44a4a4997fd426ae4ba90b78
vridhi_jain_590014870 <vridhi.aditijain@gmail.com>
2025-07-06 20:50:48 +0530

    Solution To Day21 Debugging Ques 1,2

---
+++ Quiz_3/Vridhi_Jain_590014870/Quiz_3_Debugging_Question_2.py
@@ -0,0 +1,17 @@
+# 
+def sort_colors(nums):
+    left, current, right = 0, 0, len(nums) - 1
+    while current <= right:
+        if nums[current] == 0:
+            nums[left], nums[current] = nums[current], nums[left]
+            left += 1
+            current += 1
+        elif nums[current] == 1:
+            current += 1
+        else:
+            nums[current], nums[right] = nums[right], nums[current]
+            right -= 1
+    return nums
+
+nums = [2, 0, 2, 1, 1, 0]
+print(sort_colors(nums))
