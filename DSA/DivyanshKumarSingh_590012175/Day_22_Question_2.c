badf011c8e7ca4bafe822238dc20d9a4880172c8
Divyansh <divyanshkumarsingh29@gmail.com>
2025-07-07 22:34:45 +0530

    Submitted By Divyansh Kumar Singh 590012175

--- DSA/DivyanshKumarSingh_590012175/Day_22_Question_2.c
+++ DSA/DivyanshKumarSingh_590012175/Day_22_Question_2.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Node structure
 struct Node
 {
     int data;
     struct Node *next;
 };
 
-// Insert at end
 struct Node *insertEnd(struct Node *head, int val)
 {
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
@@ -26,7 +24,6 @@ struct Node *insertEnd(struct Node *head, int val)
     return head;
 }
 
-// Count key frequency
 int countFrequency(struct Node *head, int key)
 {
     int count = 0;
