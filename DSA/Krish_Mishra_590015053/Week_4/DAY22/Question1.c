ce01ffd3b0d0f345fd6e9c87786954ad90ea051d
KRISH MISHRA <ashamishra122098@gmail.com>
2025-07-07 22:08:04 +0530

    Create Question1.c

---
+++ DSA/Krish_Mishra_590015053/Week_4/DAY22/Question1.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the end
+void insertEnd(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
+}
+
+// Function to find the middle node using two pointers
+struct Node* findMiddle(struct Node* head) {
+    struct Node* slow = head;
+    struct Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+// Function to print the list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d", head->data);
+        if (head->next != NULL) printf("->");
+        head = head->next;
+    }
+    printf("\n");
+}
+
+// Main function
+int main() {
+    struct Node* head = NULL;
+    int n, val;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        insertEnd(&head, val);
+    }
+
+    printf("Linked List: ");
+    printList(head);
+
+    struct Node* middle = findMiddle(head);
+    if (middle)
+        printf("Middle node: %d\n", middle->data);
+
+    return 0;
+}
