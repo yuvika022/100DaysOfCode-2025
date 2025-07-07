ce3eb9e28ed0888a3fc031dea8f866118d1ca759
Devaansh kushwaha <devaanshkushwaha06@gmail.com>
2025-07-07 21:24:26 +0530

    Create Question1.c

---
+++ DSA/DevaanshKushwaha_590012161/Day22/Question1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a linked list node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the end
+void insertEnd(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        struct Node* temp = *head;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = newNode;
+    }
+}
+
+// Function to find and print the middle node
+void printMiddle(struct Node* head) {
+    struct Node* slow = head;
+    struct Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    if (slow != NULL)
+        printf("Middle node: %d\n", slow->data);
+}
+
+int main() {
+    struct Node* head = NULL;
+    int n, value;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        insertEnd(&head, value);
+    }
+
+    printMiddle(head);
+
+    return 0;
+}
