529475dd632e2fdc0c556435cf981a3264fecc89
Pranya13 <pranyagupta1345@gmail.com>
2025-07-08 01:42:45 +0530

    Day 23 solutions

---
+++ DSA/PranyaGupta_500126032/Day23Question2.py
@@ -0,0 +1,41 @@
+class node:
+    def __init__(self, data):
+        self.next = None
+        self.data = data
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def add(self, data):
+        new_node = node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+    def printlist(self, head):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+      
+    def deleteduplicate(self, head):
+        current = self.head
+        while current and current.next:
+            if current.data == current.next.data:
+                current.next = current.next.next
+            else:
+                current = current.next     
+        self.printlist(self.head)           
+
+
+def main():
+    user_input = input("Enter elements of the linked list (with space in between): ")
+    arr = list(map(int, user_input.strip().split()))
+    l = LinkedList()
+    for n in arr:
+        l.add(n)
+    result = l.deleteduplicate(l.head)
+if __name__ == "__main__":
+    main()
