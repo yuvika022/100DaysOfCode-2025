c97be9d94737ca88d9d533ed1fae3b03c5cf9e73
Divyansh <divyanshkumarsingh29@gmail.com>
2025-07-07 22:33:09 +0530

    Submitted By Divyansh Kumar Singh 590012175

---
+++ DSA/DivyanshKumarSingh_590012175/Day_22_Question_3.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+// Insert at end
+struct Node *insertEnd(struct Node *head, int val)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = NULL;
+
+    if (!head)
+        return newNode;
+
+    struct Node *temp = head;
+    while (temp->next)
+        temp = temp->next;
+    temp->next = newNode;
+
+    return head;
+}
+
+// Print linked list
+void printList(struct Node *head)
+{
+    while (head)
+    {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    struct Node *head = NULL;
+    int n, val;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &val);
+        head = insertEnd(head, val);
+    }
+
+    printf("Linked List: ");
+    printList(head);
+
+    return 0;
+}
